4888741d033a0da35ae1609a1429bbb59900b49d
GoodGuyGregory <greg.witt625@gmail.com>
2024-01-24 02:33:22 -0500

    adds notes for mnist reshape and finishes initial import normalization process

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 
 
 mnist data set is ignored due to github upload limits
+
+**Resources**
+
+[Image Classification with mnist dataset](https://www.kaggle.com/code/manthansolanki/image-classification-with-mnist-dataset)
+[Split into 28 x 28 vectors for inputs](https://stackoverflow.com/questions/66336415/how-to-split-an-array-into-28x28-dimension-getting-wrong-output)
--- main.py
+++ main.py
@@ -4,14 +4,40 @@ import numpy as np
 
 # imports
 def importMNIST():
-    MNIST = pd.read_csv("mnist_train.csv", header=None)
-    MNIST.head()
+    mnist_data = pd.read_csv("mnist_train.csv")
+    # collect all of the ground_truths.
+    groundTruthLables = mnist_data['label']
+    print(groundTruthLables)
+    # drop the labels of the data and then divide each array
+    inputs = mnist_data.drop("label", axis=1).to_numpy()
+    # divide each value vector by 255...
+    normalizedInputs = np.divide(inputs, 255)
+    # obtain the shape for each input to consistently maintain dotProduct
+    normalizedInputs = normalizedInputs.reshape((-1,28,28))
+    # confirms 28 x 28
+    print(normalizedInputs.shape)
+
+    print(normalizedInputs[0])
+
+
+    # need to normalize the data. which is gray scale images of numbers 1-9
+
+
+
+
+# input 785 inputs within 10 perceptrons each input is a vector of 785 ie 28x28+1 (+1 for bias)
+
+# 785 weights per perceptron
+
 
 
 
 def main():
     # read in the MNIST data:
     importMNIST()
+
+    ## define bias:
+    bias = 1
 
 
 
